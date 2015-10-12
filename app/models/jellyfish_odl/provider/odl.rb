3fc5695723a08f4fe18ceeead104f36ab4b24784
mafernando <fernando_michael@bah.com>
2015-10-12 12:18:21 -0500

    Remove irrelevant comment

--- app/models/jellyfish_odl/provider/odl.rb
+++ app/models/jellyfish_odl/provider/odl.rb
@@ -18,7 +18,6 @@ module JellyfishOdl
       private
 
       def client
-        # TODO: IMPLEMENT DOCKER CLIENT - https://docs.docker.com/reference/api/docker_remote_api/
         @client ||= SimpleClient.new
       end
     end
