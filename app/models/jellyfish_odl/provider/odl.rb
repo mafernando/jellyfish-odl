2d80bc0d06cce8d125bbbb27977103c334a912b5
mafernando <fernando_michael@bah.com>
2015-10-12 15:29:02 -0500

    Remove simple client and plug in Httparty for REST

--- app/models/jellyfish_odl/provider/odl.rb
+++ app/models/jellyfish_odl/provider/odl.rb
@@ -1,24 +1,46 @@
 module JellyfishOdl
   module Provider
-    class SimpleClient < ::Provider
-      def initialize
-        true
-      end
-
-      def settings
-        {}
-      end
-    end
-
     class Odl < ::Provider
       def network_topology
-        [{ host: 'Host 1', ip: '10.0.0.1' }, { host: 'Host 2', ip: '10.0.0.2' }]
+        auth = {:username => settings[:username], :password => settings[:password]}
+
+        base_url = "http://#{settings[:ip_address]}:#{settings[:port]}"
+
+        headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
+        odl_module = 'restconf/operational/opendaylight-inventory:nodes/'
+
+        composite_url = "#{base_url}/#{odl_module}"
+
+        response = client.get(composite_url, :basic_auth => auth, :headers => headers)
+
+        nodes = []
+
+        begin
+          response['nodes']['node'].each { |x|
+            node = {}
+
+            i = 0
+            node_connector = []
+            x['node-connector'].each { |y|
+              node_connector << { :id => y['id'] }
+            }
+
+            node[:node_connector] = node_connector
+
+            nodes << { x['id'] => node }
+          }
+        rescue
+          p 'Could not parse output'
+        end
+
+        nodes
       end
 
       private
 
       def client
-        @client ||= SimpleClient.new
+        @client ||= HTTParty
       end
     end
   end
